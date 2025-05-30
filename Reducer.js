f4bae9629fa237eeeb20cc7a6ea9292edc3dd571
Mashhod Bin Shahid <mashhodbinshahid11@gmail.com>
2025-05-30 11:56:57 +0500

    Reducer.js

--- Reducer.js
+++ Reducer.js
@@ -1,17 +1,17 @@
 export const reducer = (state, action) => {
   switch (action.type) {
-    // case "USER_LOADER_NAV_FALSE": {
-    //   console.log('reducer USER_LOADER_NAV_FALSE ', state);
-    //   return {isLogin: state?.isLogin , user: action.payload, isLoader: false }
-    // }
-    // case "USER_LOADER_NAV_TRUE": {
-    //   console.log('reducer USER_LOADER_NAV_TRUE ', state);
+    case "USER_LOADER_NAV_FALSE": {
+      console.log('reducer USER_LOADER_NAV_FALSE ', state);
+      return {isLogin: state?.isLogin , user: action.payload, isLoader: false }
+    }
+    case "USER_LOADER_NAV_TRUE": {
+      console.log('reducer USER_LOADER_NAV_TRUE ', state);
       
-    //   return { isLogin: state?.isLogin , user: action.payload, isLoader: true }
-    // }
-    // case "USER_LOADER": {
-    //   return { isLogin: false , user: {}, isLoader: true }
-    // }
+      return { isLogin: state?.isLogin , user: action.payload, isLoader: true }
+    }
+    case "USER_LOADER": {
+      return { isLogin: false , user: {}, isLoader: true }
+    }
     case "USER_LOGIN": {
       return { isLogin: true , user: action.payload, isLoader: false }
     }
